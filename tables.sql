478ed472857c501476fb03348c77e28f7b3cd5f6
valleyrem <remrem2310@gmail.com>
2024-02-12 11:45:49 +0300

    add migrations

---
+++ tables.sql
@@ -0,0 +1,20 @@
+-- users_info
+CREATE TABLE users_info (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER UNIQUE, -- или PRIMARY KEY,
+    age INTEGER,
+    sex TEXT,
+    lang CHARACTER VARYING
+);
+
+-- user_moods
+CREATE TABLE user_moods (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    mood INTEGER,
+    date DATE,
+    time TIME WITHOUT TIME ZONE,
+    day_of_week CHARACTER VARYING,
+    FOREIGN KEY (user_id)
+    REFERENCES users_info(user_id)
+);
